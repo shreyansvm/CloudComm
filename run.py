283c1e8d94eecd9a139ec0029eb06d3e72126945
shreyansvm <shreyans.mulkutkar@gmail.com>
2018-03-11 23:21:51 -0700

    Trying to create short conversation

--- run.py
+++ run.py
@@ -1,22 +1,41 @@
+# /usr/bin/env python
 
 '''
 This acts like a server
 '''
-
-# /usr/bin/env python
 from flask import Flask, request
 from twilio.twiml.messaging_response import MessagingResponse
+# My common methods
+from commonMethods  import *
+
+# This can be any service/product/customer care/emergency services number
+# twilioNumber    = "yyy"
+# customerNumber  = "xxx"
 
 app = Flask(__name__)
 
 @app.route("/sms", methods=['GET', 'POST'])
-def sms_ahoy_reply():
-    """Respond to incoming messages with a friendly SMS."""
+def sms_reply():
+    """Respond to incoming messages and create a conversation"""
+    txMsgBody       = ""
+
+    rxFromNumber    = request.values.get('From')
+    rxMsgBody       = request.values.get('Body')
+    rxMsgBody       = rxMsgBody.capitalize()
+
+    if any( [rxMsgBody == "Yes", rxMsgBody == "Y"] ):
+        txMsgBody += "Great! "
+    elif any( [rxMsgBody == "No", rxMsgBody == "N"] ):
+        txMsgBody += "Okay. I'll check with next in line"
+    else:
+        txMsgBody += "Do you mind getting with Shrek? "
+
     # Start our response
     resp = MessagingResponse()
 
     # Add a message
-    resp.message("Ahoy! Thanks so much for your message.")
+    txMsgBody += greetingString('end')
+    resp.message(txMsgBody)
 
     return str(resp)
 
--- send_sms.py
+++ send_sms.py
@@ -3,12 +3,14 @@ This acts like a client
 '''
 
 from twilio.rest import Client
+# My common methods
+from commonMethods  import *
 
-myAccSid    = "BBBBBBBB"
-myAccToken  = "AAAAAAAA"
-fromNumber  = "yyy"
-smulkutk    = "xxx"
-toNumber    = smulkutk
+# myAccSid    = "BBBBBBBB"
+# myAccToken  = "AAAAAAAA"
+# fromNumber  = "yyy"
+# smulkutk    = "xxx"
+# toNumber    = smulkutk
 
 account_sid = myAccSid
 auth_token  = myAccToken
@@ -16,8 +18,14 @@ auth_token  = myAccToken
 client = Client(account_sid, auth_token)
 
 message = client.messages.create(
-    to  =toNumber,
-    from_=fromNumber,
-    body="Hello from BytePython!")
+    to  = toNumber,
+    from_= fromNumber,
+    body = "Hello from BytePython! Can you get Samosa tomorrow? Reply : YES(Y) OR NO(N)")
 
 print(message.sid)
+
+'''
+ Resources :
+    Twilio Messaging API : https://www.twilio.com/docs/api/messaging
+
+'''
