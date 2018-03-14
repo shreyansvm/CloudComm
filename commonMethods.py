79b24839912b4835060e5f35f13e8114047601a5
shreyansvm <shreyans.mulkutkar@gmail.com>
2018-03-14 00:34:12 -0700

    added msgBodyString method

--- commonMethods.py
+++ commonMethods.py
@@ -1,6 +1,6 @@
-# Add all common methods
-
 import datetime
+from datetime import date
+import calendar
 
 '''
 Returns Greetings string to start or end with.
@@ -32,3 +32,32 @@ def greetingString(type):
         greetings = "Thank you, " + greetings + ". "
 
     return greetings
+
+def msgBodyString(twoHosts, type):
+    # TODO : if twoHosts is empty then raise an exception.
+
+    msgBody = ""
+    turn = ", it's your turn to get snacks. "
+    replyFormat = "Reply:\n" \
+                  "NO - If you are NOT available.  \n" \
+                  "OK - If one of you is available and the other can just contribute\n"
+
+    host_1 = twoHosts[0].split(' ')
+    host_2 = twoHosts[1].split(' ')
+    bothHosts = "Hello "+str(host_1[0])+" and "+str(host_2[0])+":\n"
+
+    my_date = date.today()
+    snacksDay = ''  ;# Today or Tomorrow
+    if calendar.day_name[my_date.weekday()] == 'Monday' or calendar.day_name[my_date.weekday()] == 'Wednesday':
+        snacksDay = 'Tomorrow'
+    elif calendar.day_name[my_date.weekday()] == 'Tuesday' or calendar.day_name[my_date.weekday()] == 'Thursday':
+        snacksDay = 'REMINDER : Today'
+
+    if type == 'start':
+        msgBody = msgBody + bothHosts + snacksDay + turn + replyFormat
+
+
+    return msgBody
+
+#print greetingString('end')
+#print msgBodyString(['Manish Jain Manish.Jain@alcatel-lucent.com 0\n', 'Karunendra Karumanchi Karunendra.Karumanchi@alcatel-lucent.com 0\n'], 'start')
